1c203bdfb4f60260c9d4da1245f86e3186017419
gabzon <gab.zambrano@gmail.com>
2016-01-14 23:37:08 +0100

    25

--- chronologic.php
+++ chronologic.php
@@ -41,8 +41,10 @@
 
     <?php foreach ($categories as $cat): ?>
         Cat name: <?= get_cat_name($cat) . '------'; ?>
-        <?php if (get_cat_name($cat) != 'NON CLASSÉ ' ): ?>
+        <?php if (get_cat_name($cat) != 'Non classé' ): ?>
             NOT TRUE
+        <?php else: ?>
+            TRUE
         <?php endif; ?>
         <?php if (get_cat_name($cat) != 'EXPOSITIONS' && get_cat_name($cat) != 'NON CLASSÉ'): ?>
             <a href="<?= get_category_link( $cat ); ?>">
