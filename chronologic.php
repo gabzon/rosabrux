a1a3c10ffc59c9c20a79c730255a7c60509ca3f7
gabzon <gab.zambrano@gmail.com>
2016-01-14 21:04:41 +0100

    17

--- chronologic.php
+++ chronologic.php
@@ -40,7 +40,7 @@
     <?php //piklist::pre($categories); ?>
 
     <?php foreach ($categories as $cat): ?>
-        <?php if (get_cat_name($cat) !== 'EXPOSITIONS' || get_cat_name($cat) !== 'NON CLASSÉ'): ?>
+        <?php if (get_cat_name($cat) != 'EXPOSITIONS' || get_cat_name($cat) != 'NON CLASSÉ'): ?>
             <a href="<?= get_category_link( $cat ); ?>">
                 <?= get_cat_name($cat); ?>
             </a>
