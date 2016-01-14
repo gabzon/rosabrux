b2fd0156a30b5b1c139bf03a46597aa323956aa9
gabzon <gab.zambrano@gmail.com>
2016-01-14 19:57:56 +0100

    display single title on chronology

--- chronologic.php
+++ chronologic.php
@@ -15,24 +15,30 @@
         <?php
 
         $expos = get_posts(
-            array(
-                'posts_per_page' => -1,
-                'post_type' => 'post',
-                'tax_query' => array(
-                    array(
-                        'taxonomy' => 'expo_year',
-                        'field' => 'term_id',
-                        'terms' => $year->term_id,
-                    )
+        array(
+            'posts_per_page' => -1,
+            'post_type' => 'post',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'expo_year',
+                    'field' => 'term_id',
+                    'terms' => $year->term_id,
                 )
             )
-        );?>
+        )
+    );?>
 
-        <?php foreach ($expos as $expo): ?>
-            <a href="<?php echo esc_url(get_permalink( $expo->ID )); ?>" style="text-transform:uppercase;">
-                <?php echo $expo->post_title; ?>
-            </a>
-            <br>
-        <?php endforeach; ?>
+    <?php foreach ($expos as $expo): ?>
+        <a href="<?php echo esc_url(get_permalink( $expo->ID )); ?>" style="text-transform:uppercase;">
+            <?php $title = ''; ?>
+            <?php if ($title != '') : ?>
+                <?php if ($title == $expo->post_title): ?>
+                    <?= $title; ?>
+                    <?php $title =  $expo->post_title ?>
+                <?php endif; ?>
+            <?php endif; ?>
+        </a>
+        <br>
     <?php endforeach; ?>
+<?php endforeach; ?>
 </div>
